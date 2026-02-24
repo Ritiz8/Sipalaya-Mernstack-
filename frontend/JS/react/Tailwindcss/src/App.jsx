21dfb6fd862ff140b4e8766dd4c08df81e75c35f
Ritiz <ritizthaiba8@gmail.com>
2026-02-24 10:47:14 +0545

    react js

--- frontend/JS/react/Tailwindcss/src/App.jsx
+++ frontend/JS/react/Tailwindcss/src/App.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
 import Card from './component/Card'
 const App = () => {
-  function btnClick(){
-    comsole.log("button is clicked")
-  }
-  return (
-    // <div className='flex flex-wrap justify-center bg-amber-900 md:bg-blue-500 lg:bg-green-800'>
-    //   <Card />  
-    //   <Card />
-    //   <Card />
-    //   <Card />
-    //   <Card />   
-    // </div>
-    <div>
-    <button onClick={()=>{
-      
-    }}></button>
-
+     return (
+    <div className='flex flex-wrap justify-center bg-amber-900 md:bg-blue-500 lg:bg-green-800'>
+      <Card />  
+      <Card />
+      <Card />
+      <Card />
+      <Card />   
     </div>
   )
 }
